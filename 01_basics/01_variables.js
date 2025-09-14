6f7be68efe47b669c90bcf5509c6066fd8bef705
purvesh0107 <83306586+purvesh0107@users.noreply.github.com>
2025-09-14 12:09:41 +0000

    variable and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 107098
+let accountEmail = "purvesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+//accountId = 321
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+console.log(accountId);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//alert(3+3) // we are using nodeJS, not browser
+
+let name = "Purvesh"
+let age = 27
+let isLoogedIn = true
+let isNull
+
+console.log(typeof isNull)
