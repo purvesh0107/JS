15112e7974e357bd1e90779fd28bb687d0464b8e
purvesh0107 <83306586+purvesh0107@users.noreply.github.com>
2025-09-14 13:23:28 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = undefined
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 345
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+
+
